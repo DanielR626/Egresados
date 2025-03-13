31bdee2ecef09cf4d2eea285a30a17da1be0bd15
Daniel Caceres <dcacerest6@gmail.com>
2025-03-13 15:30:38 -0500

    prueba 3
    prueba 3

--- app/Models/User.php
+++ app/Models/User.php
@@ -14,6 +14,7 @@ class User extends Authenticatable implements MustVerifyEmail
     /** @use HasFactory<\Database\Factories\UserFactory> */
     
     /** Hola Muchachos*/
+    /** universidad mariana*/
     use HasFactory, Notifiable;
 
     /**
