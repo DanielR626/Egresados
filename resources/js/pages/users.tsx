60b1e28398c7a295387f7afd18f40bde76d33d77
Roberth <roberthrs109@gmail.com>
2025-03-15 11:22:38 -0500

    Se agrego una tabla de usuarios
    Se agrego una tabla de usuarios

--- resources/js/pages/users.tsx
+++ resources/js/pages/users.tsx
@@ -3,6 +3,8 @@ import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/react';
+import TableUsers from '@/components/TableUsers';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 const breadcrumbs: BreadcrumbItem[] = [
@@ -12,13 +14,21 @@ const breadcrumbs: BreadcrumbItem[] = [
     },
 ];
 
+
+const darkTheme = createTheme({
+    palette: {
+        mode: 'dark',
+    },
+});
+
 export default function Dashboard() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Dashboard" />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
-                <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-                </div>  
+                <ThemeProvider theme={darkTheme}>
+                    <TableUsers />
+                </ThemeProvider>
             </div>
         </AppLayout>
     );
