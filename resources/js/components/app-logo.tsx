b4a8febe4077b696eb856a74cbcc4e1257e5de03
Roberth <roberthrs109@gmail.com>
2025-03-17 00:26:17 -0500

    Mejoras visuales logos

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -2,13 +2,9 @@ import AppLogoIcon from './app-logo-icon';
 
 export default function AppLogo() {
     return (
-        <>
-            <div className="bg-sidebar-primary text-sidebar-primary-foreground flex aspect-square size-10 items-center justify-center rounded-md">
-            <a href="https://ibb.co/9H8CdPdQ"><img src="https://i.ibb.co/9H8CdPdQ/Escudo-Universidad-Mariana-removebg-preview.png" alt="Escudo-Universidad-Mariana-removebg-preview"/></a>
-            </div>
-            <div className="ml-1 grid flex-1 text-left text-sm">
-                <span className="mb-0.5 truncate leading-none font-semibold">Universidad Mariana</span>
-            </div>
-        </>
+        <div className='flex justify-center items-center size-40'>
+            <img src=" https://www.umariana.edu.co/web2020/Logo-UniversidadMariana2019.png" />
+        </div>
+
     );
 }
